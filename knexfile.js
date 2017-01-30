ea8a51ab0e510bb7a9fd1cd63f854d429f6d5185
oliviaridgley <oliviatateridgley@gmail.com>
2017-01-30 12:52:29 -0700

    deployment commit

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,18 @@
 // Update with your config settings.
+'use strict';
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
-
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/classifieds_test'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'classifieds_dev'
+  }
 };
